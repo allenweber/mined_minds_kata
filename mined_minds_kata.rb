e8ffdab73b3339212daf479bbd648042829ad268
allenweber <weberallen1@gmail.com>
2017-01-18 10:50:06 -0800

    Add files via upload

---
+++ mined_minds_kata.rb
@@ -0,0 +1,11 @@
+def minedmindskata(parameter1)
+	if parameter1 ==3
+		"Mind"
+	else
+		if parameter1 ==5
+			"Mined"
+		else
+			parameter1
+		end
+	end
+end
---
+++ test_mined_minds_kata.rb
@@ -0,0 +1,30 @@
+require "minitest/autorun"
+require_relative "mined_minds_kata.rb"
+
+
+class TestMinedMindsKata < MiniTest::Test
+    def test_1_returns_1
+    	assert_equal(1,1)
+    end
+
+    def test_function_returns_1
+    	assert_equal(1,minedmindskata(1))
+    end
+
+    def test_function_returns_2
+    	assert_equal(2,minedmindskata(2))
+    end
+
+    def test_function_return_mind
+    	assert_equal("Mind",minedmindskata(3))
+    end
+
+    def test_function_returns_5
+    	assert_equal("Mined",minedmindskata(5))
+    end
+
+    #def test_function_returns_6
+    #	assert_equal()
+
+end
+
